ad432f1eaeac612e024ac63c7243b225aa06b3fa
Markus Olsen <molsen13@gmail.com>
2015-02-24 18:10:42 -0700

    Add provided style

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,12 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
 end
