8f068ff6fb64e8301f1a888cb4440475c4056956
Markus Olsen <molsen13@gmail.com>
2015-02-25 17:37:56 -0700

    Resizing images: mission complete

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings, dependent: :destroy
-  has_attached_file :image
+  has_attached_file :image, styles: { medium: "300x300>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
 
