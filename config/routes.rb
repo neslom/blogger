e59b84c50b64f97b70b1cc17b8ebb1a6b2f94a7c
Markus Olsen <molsen13@gmail.com>
2015-02-25 21:55:59 -0700

    Possibly the final commit for this project

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,10 @@ Rails.application.routes.draw do
 
   resources :authors
 
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+  get 'login' => 'author_sessions#new'
+
+  get 'logout' => 'author_sessions#destroy'
+
 end
